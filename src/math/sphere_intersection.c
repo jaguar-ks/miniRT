ebaf8397c90301f76dfd82c4ef9e3a2b62084a36
jaguar-ks <fahdamineksouss@gmail.com>
2023-06-05 22:35:10 +0100

    FIX : normed the intersection file

--- src/math/sphere_intersection.c
+++ src/math/sphere_intersection.c
@@ -6,15 +6,15 @@
 /*   By: faksouss <faksouss@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 09:36:28 by faksouss          #+#    #+#             */
-/*   Updated: 2023/06/05 22:27:33 by faksouss         ###   ########.fr       */
+/*   Updated: 2023/06/05 22:34:41 by faksouss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../../inc/minirt.h"
+#include "../../inc/minirt.h"
 
-t_vctr  sub_vctr(t_vctr v1, t_vctr v2)
+t_vctr	sub_vctr(t_vctr v1, t_vctr v2)
 {
-    return ((t_vctr){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z});
+	return ((t_vctr){v1.x - v2.x, v1.y - v2.y, v1.z - v2.z});
 }
 
 int	check_sph_intersection(t_sphere *sp, t_ray *ray, double *t)
@@ -25,7 +25,7 @@ int	check_sph_intersection(t_sphere *sp, t_ray *ray, double *t)
 	double	t2;
 	double	delta;
 
-    v = sub_vctr(ray->org, sp->crd);
+	v = sub_vctr(ray->org, sp->crd);
 	abc.x = dot_prdct(ray->drct, ray->drct);
 	abc.y = 2 * dot_prdct(v, ray->drct);
 	abc.z = dot_prdct(v, v) - pow(sp->dmt / 2, 2);
@@ -34,7 +34,7 @@ int	check_sph_intersection(t_sphere *sp, t_ray *ray, double *t)
 		return (0);
 	t1 = (-abc.y + sqrt(delta)) / (2 * abc.x);
 	t2 = (-abc.y - sqrt(delta)) / (2 * abc.x);
-    if (t1 - t2 < EPS)
+	if (t1 - t2 < EPS)
 		*t = t1;
 	else
 		*t = t2;
@@ -44,9 +44,9 @@ int	check_sph_intersection(t_sphere *sp, t_ray *ray, double *t)
 int	check_pln_intersection(t_plane *pl, t_ray *ray, double *t)
 {
 	double	t1;
-    t_vctr  v;
+	t_vctr	v;
 
-    v = sub_vctr(ray->org, pl->crd);
+	v = sub_vctr(ray->org, pl->crd);
 	t1 = (dot_prdct(sub_vctr(pl->crd, ray->org), pl->nrml_vctr))
 		/ dot_prdct(pl->nrml_vctr, ray->drct);
 	if (t1 > EPS)
@@ -54,31 +54,34 @@ int	check_pln_intersection(t_plane *pl, t_ray *ray, double *t)
 	return (0);
 }
 
-int check_cyl_intersection(t_cylender *cy, t_ray *ray, double *t)
+int	check_cyl_intersection(t_cylender *cy, t_ray *ray, double *t)
 {
-    t_vctr  s;
-    t_vctr  v;
-    double  t1;
-    double  t2;
-    double  delta;
+	t_vctr	s;
+	t_vctr	v;
+	t_vctr	d;
 
-    cy->nrml_vctr = unit_vctr(cy->nrml_vctr);
-    v = sub_vctr(ray->org, cy->crd);
-    s.x = dot_prdct(ray->drct, ray->drct) - pow(dot_prdct(ray->drct, cy->nrml_vctr), 2);
-    s.y = 2 * (dot_prdct(ray->drct, v) - (dot_prdct(ray->drct, cy->nrml_vctr) * dot_prdct(v, cy->nrml_vctr)));
-    s.z = dot_prdct(v, v) - pow(dot_prdct(v, cy->nrml_vctr), 2) - pow(cy->dmt / 2, 2);
-    delta = pow(s.y, 2) - 4 * (s.x * s.z);
-    if (delta < EPS)
-        return (0);
-    t1 = -s.y - sqrt(delta) / 2 * s.x;
-    t2 = -s.y + sqrt(delta) / 2 * s.x;
-    if (t2 < EPS)
-        return (0);
-    t1 = dot_prdct(ray->drct, cy->nrml_vctr) * t1 + dot_prdct(v, cy->nrml_vctr);
-    t2 = dot_prdct(ray->drct, cy->nrml_vctr) * t2 + dot_prdct(v, cy->nrml_vctr);
-    if (t2 >= EPS && t2 <= cy->hgt)
-        return (*t = (-s.y + sqrt(delta)) / (2 * s.x), 1);
-    if (t1 >= EPS && t1 <= cy->hgt)
-        return (*t = (-s.y - sqrt(delta)) / (2 * s.x), 1);
-    return (0);
+	cy->nrml_vctr = unit_vctr(cy->nrml_vctr);
+	v = sub_vctr(ray->org, cy->crd);
+	s.x = dot_prdct(ray->drct, ray->drct) - pow(dot_prdct(ray->drct,
+			cy->nrml_vctr), 2);
+	s.y = 2 * (dot_prdct(ray->drct, v) - (dot_prdct(ray->drct, cy->nrml_vctr)
+			* dot_prdct(v, cy->nrml_vctr)));
+	s.z = dot_prdct(v, v) - pow(dot_prdct(v, cy->nrml_vctr), 2) - pow(cy->dmt
+		/ 2, 2);
+	d.x = pow(s.y, 2) - 4 * (s.x * s.z);
+	if (d.x < EPS)
+		return (0);
+	d.y = -s.y - sqrt(d.x) / 2 * s.x;
+	d.z = -s.y + sqrt(d.x) / 2 * s.x;
+	if (d.z < EPS)
+		return (0);
+	d.y = dot_prdct(ray->drct, cy->nrml_vctr) * d.y + dot_prdct(v,
+		cy->nrml_vctr);
+	d.z = dot_prdct(ray->drct, cy->nrml_vctr) * d.z + dot_prdct(v,
+		cy->nrml_vctr);
+	if (d.z >= EPS && d.z <= cy->hgt)
+		return (*t = (-s.y + sqrt(d.x)) / (2 * s.x), 1);
+	if (d.y >= EPS && d.y <= cy->hgt)
+		return (*t = (-s.y - sqrt(d.x)) / (2 * s.x), 1);
+	return (0);
 }
